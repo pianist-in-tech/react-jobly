35702180e836ad9083510b0a80d77372dcab8beb
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2024-03-27 23:02:16 -0600

    multiple changes, mostly in frontend

--- backend/config.js
+++ backend/config.js
@@ -13,7 +13,7 @@ const PORT = +process.env.PORT || 3001;
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
       ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+      : process.env.DATABASE_URL || "postgresql:///jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import LoadingSpinner from "./common/LoadingSpinner";
 import JoblyApi from "./api/api";
 import UserContext from "./auth/UserContext";
 import jwt from "jsonwebtoken";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Key name for storing token in localStorage for "remember me" re-login
 export const TOKEN_STORAGE_ID = "jobly-token";
